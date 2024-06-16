aa46dce5ab071f523c21f7deeea8d62d0e190693
quantumoptics <221503020@smail.nju.edu.cn>
2024-06-16 17:50:23 +0800

    Update numerical_integral.py

--- H2 fresnel直边衍射/numerical_integral.py
+++ H2 fresnel直边衍射/numerical_integral.py
@@ -1,45 +1,49 @@
-## *args 是打包和解包
-# 在传递args的时候不加*
-# 例如integral接受调用处的 args = (a,b,...)
-# 传递给 integral_calculate(...,args,...)
-# integral_calculate 调用 func 需要解包，fun(...,*args,...)
-
 import numpy as np
-import matplotlib.pyplot as plt
+### 1D integral
+def integral_subcalculate(fun,x_start,x_end,args = ()):
+    # calculate a small integrate over xspan
 
-#def fun(x,a):
-#    f = np.exp(-a*x**2)
-#    return f
-## lambda: f
-def fun(x,a,b):
-    f = b*np.exp(-a*x**2)
-    return f
+    Xlist4 = np.linspace(0,1,5,endpoint = True)*(x_end - x_start) + x_start
+    i4 = fun(Xlist4,*args)
+    Co_4 = np.array([7,32,12,32,7])
 
-def integral_calculate(fun,xspan, args=() ,h_step = 1e-3,TOL = 1e-4):
-    x0 = xspan[0]
-    x1 = xspan[1]
+    I = (np.sum(Co_4*i4,axis = None))*(x_end - x_start)/90
+    return I
 
-    h = h_step/10 
+def integral_operate(fun,xspan,args = (),TOL = 1e-6):
+    N = 6
+    int_space = np.linspace(xspan[0],xspan[1],N,endpoint = True)
     result = 0
-    while(x0<x1):
-        I1 = h*(fun(x0,*args) +4*fun(x0+h/2,*args) +fun(x0+h,*args) )/6 # 
-        I2 = h*( fun(x0,*args) + 3*fun(x0+h/3,*args) + 3*fun(x0+2*h/3,*args) + fun(x0+h,*args) )/8
-        
-        erf = np.abs(I2 - I1)
-        e = erf/np.max([np.abs(I2),1])
+    j = 0
+    n = 4
+    while(j<(N-1)): ## integral in int_space
+        n = np.max([N//2,n//2])
+        erf = 1
+        result_0 = integral_subcalculate(fun,int_space[j],int_space[j+1],args)
+        while(erf >= TOL): ## self_adjust integral in span: { int_space[j],int_space[j+1] }
+            sub_space = np.linspace(int_space[j],int_space[j+1],n,endpoint = True); 
+            result_1 = 0
+            k = 0
+            while(k<(n-1)): ## integral in sub_space
+                result_1 += integral_subcalculate(fun,sub_space[k],sub_space[k+1],args)
+                k += 1
+            n *= 2
 
-        if e <= TOL:
-            x0 += h
-            result += I2
-            h = np.min([h*(TOL/(e+TOL**2))**0.3,h_step]) 
-        else:
-            h *= 0.8*(TOL/e)**0.5
-            if h < 1e-9:
-                ValueError (f'function singular at x = {x0}')
+            erf = np.abs(result_1 - result_0)/np.max([1,np.abs(result_1)])
+            result_0 = result_1 
 
+        result += result_0
+        j += 1
+    
     return result
 
-def integral(fun,xspan,args = (),h_step = 1e-3,TOL = 1e-4):
+def integral(fun,xspan,args = (),TOL = 1e-6):
+    """ numerical integral
+        support complex function: np.sqrt(-1+0j) = 1j
+        fun:  function to integrate,
+        xpans: integral span, 
+        TOL: value to control numerical error
+    """
     if xspan[1] == xspan[0]:
         return 0
     
@@ -62,81 +66,200 @@ def integral(fun,xspan,args = (),h_step = 1e-3,TOL = 1e-4):
     match process:
         case [0,1]:
             step = 10
-            result = integral_calculate(fun,[x0,x0+step],args,TOL)
+            result = integral_operate(fun,[x0,x0+step],args,TOL)
             e = 1
-            x0 += step
             while(e>=TOL):
-                add = integral_calculate(fun,[x0,x0+step],args,TOL)
-                result += add
                 x0 += step
+                add = integral_operate(fun,[x0,x0+step],args,TOL)
+                result += add
                 e = np.abs( add/np.max([np.abs(result),1])  )
 
         case [-1,0]:
             step = 10
-            result = integral_calculate(fun,[x1-step,x1],args,TOL)
+            result = integral_operate(fun,[x1-step,x1],args,TOL)
             e = 1
-            x1 -= step
             while(e>=TOL):
-                add = integral_calculate(fun,[x1-step,x1],args,TOL)
-                result += add
                 x1 -= step
+                add = integral_operate(fun,[x1-step,x1],args,TOL)
+                result += add
                 e = np.abs( add/np.max([np.abs(result),1])  )
 
         case [-1,1]:
             # [0,1]
             x0 = 0
             step = 10
-            result_f = integral_calculate(fun,[x0,x0+step],args,TOL)
+            result_f = integral_operate(fun,[x0,x0+step],args,TOL)
             e = 1
             x0 += step
             while(e>=TOL):
-                add = integral_calculate(fun,[x0,x0+step],args,TOL)
-                result_f += add
                 x0 += step
+                add = integral_operate(fun,[x0,x0+step],args,TOL)
+                result_f += add
                 e = np.abs( add/np.max([np.abs(result_f),1])  )
 
             #[-1,0]
             x1 = 0
-            result_b = integral_calculate(fun,[x1-step,x1],args,TOL)
+            result_b = integral_operate(fun,[x1-step,x1],args,TOL)
             e = 1
-            x1 -= step
             while(e>=TOL):
-                add = integral_calculate(fun,[x1-step,x1],args,TOL)
-                result_b += add
                 x1 -= step
+                add = integral_operate(fun,[x1-step,x1],args,TOL)
+                result_b += add
                 e = np.abs( add/np.max([np.abs(result_b),1])  )
 
             result = result_f + result_b
 
         case _:
-            result  = integral_calculate(fun,[x0,x1],args,TOL)
+            result = integral_operate(fun,[x0,x1],args,TOL)
 
-    ## return 
-    return (result*sgn)  
+    return (result*sgn)
 
-def test():
-    a = 0.5 # users parametera
-    xspan = np.array([-10,10]) # integral span
-
-    #fun = lambda x,a: x**4*np.exp(-a*x**2)    
-    xlist = np.linspace(xspan[0],xspan[1],5)
-    xlist[-1] = np.infty
-    xlist[0] = -np.infty
-    relist = np.empty_like(xlist)
+### 2D integral
+def integral2D_subcalculate(yxfun,funy_down,funy_up,x_start,x_end,args=()):
+    xlist = np.linspace(x_start,x_end,5,endpoint = True)
+    Co_4 = np.array([7,32,12,32,7])
+    result = 0
     i = 0
-    while (i<np.size(xlist)):
-        result = integral(fun,[xlist[0],xlist[i]],args = (a,1,))
-        relist[i] = result
+    while(i<5):
+        int_y = integral(yxfun,[funy_down(xlist[i]),funy_up(xlist[i])],(xlist[i],*args))
+        result += int_y*Co_4[i]
         i += 1
 
-    ## figure
-    plt.figure(1)
-    plt.scatter(xlist,relist,label = 'area integral')
-    plt.plot(xlist,np.sqrt(np.pi/(a))*np.ones_like(xlist),'r',label = 'final line')
-    plt.legend()
-    plt.title('self-adjusting numerical integral')
-    plt.show()
+    return (result*(x_end - x_start)/90)
+
+def integral2D_operate(yxfun,funy_down,funy_up,xspan,args=(),TOL = 1e-6):
+    N = 6
+    int_xspace = np.linspace(xspan[0],xspan[1],N,endpoint = True)
+    result = 0
+    j = 0
+    n = 4
+    while(j<(N-1)): ## integral in int_space
+        n = np.max([N//2,n//2])
+        erf = 1
+        result_0 = integral2D_subcalculate(yxfun,funy_down,funy_up,int_xspace[j],int_xspace[j+1],args)
+        while(erf >= TOL): ## self_adjust integral in span: { int_space[j],int_space[j+1] }
+            sub_space = np.linspace(int_xspace[j],int_xspace[j+1],n,endpoint = True); 
+            result_1 = 0
+            k = 0
+            while(k<(n-1)): ## integral in sub_space
+                result_1 += integral2D_subcalculate(yxfun,funy_down,funy_up,sub_space[k],sub_space[k+1],args)
+                k += 1
+            n *= 2
+
+            erf = np.abs(result_1 - result_0)/np.max([1,np.abs(result_1)])
+            result_0 = result_1 
+
+        result += result_0
+        j += 1
+    
+    return result
+
+def integral2D(yxfun,funy_down,funy_up,xspan,args = (),TOL = 1e-6):
+    """ 2D numerical integral
+        support complex function: np.sqrt(-1+0j) = 1j
+        yxfun: function to integrate, but you should use function of this form: func(y,x,*args)
+        xpans: integral span of x-axis
+        funy: funtion of y lines
+        TOL: value to control numerical error
+    """
+    if xspan[1] == xspan[0]:
+        return 0
+    
+    if xspan[1] > xspan[0]:
+        sgn = 1
+        x0 = xspan[0]
+        x1 = xspan[1]
+    else:
+        sgn = -1
+        x0 = xspan[1]
+        x1 = xspan[0]
+    
+    process = [0,0]
+    if x1 == np.infty:
+        process[1] = 1
+    if x0 == -np.infty:
+        process[0] = -1
+    
+    ## integral -- integral2D_operate(yxfun,funy_down,funy_up,xspan,args,TOL)
+    match process:
+        case [0,1]:
+            step = 10
+            result = integral2D_operate(yxfun,funy_down,funy_up,[x0,x0+step],args,TOL)
+            e = 1
+            while(e>=TOL):
+                x0 += step
+                add = integral2D_operate(yxfun,funy_down,funy_up,[x0,x0+step],args,TOL)
+                result += add
+                e = np.abs( add/np.max([np.abs(result),1])  )
+
+        case [-1,0]:
+            step = 10
+            result = integral2D_operate(yxfun,funy_down,funy_up,[x1-step,x1],args,TOL)
+            e = 1
+            while(e>=TOL):
+                x1 -= step
+                add = integral2D_operate(yxfun,funy_down,funy_up,[x1-step,x1],args,TOL)
+                result += add
+                e = np.abs( add/np.max([np.abs(result),1])  )
+
+        case [-1,1]:
+            # [0,1]
+            x0 = 0
+            step = 10
+            result_f = integral2D_operate(yxfun,funy_down,funy_up,[x0,x0+step],args,TOL)
+            e = 1
+            while(e>=TOL):
+                x0 += step
+                add = integral2D_operate(yxfun,funy_down,funy_up,[x0,x0+step],args,TOL)
+                result_f += add
+                e = np.abs( add/np.max([np.abs(result_f),1])  )
+
+            #[-1,0]
+            x1 = 0
+            result_b = integral2D_operate(yxfun,funy_down,funy_up,[x1-step,x1],args,TOL)
+            e = 1
+            while(e>=TOL):
+                x1 -= step
+                add = integral2D_operate(yxfun,funy_down,funy_up,[x1-step,x1],args,TOL)
+                result_b += add
+                e = np.abs( add/np.max([np.abs(result_b),1])  )
+
+            result = result_f + result_b
+
+        case _:
+            result = integral2D_operate(yxfun,funy_down,funy_up,xspan,args,TOL)
+
+    return (result*sgn)
+
+### 1D discrete integral
+def int_integral(ylist,xlist,order = 4,TOL = 1e-6):
+    """ discrete numerical integral, first do interpolation, and then numerical integrate
+        ylist: discrete sampling date from sensor you want to integrate over xlist 
+        xlist: span over which you want to do integration about ylist
+        order: value to control interpolate error 1 <= order <= 5
+        TOL: value to control integrate error
+    """
+    from scipy.interpolate import UnivariateSpline
+    spline = UnivariateSpline(xlist,ylist,k = order)
+
+    fun = lambda x: spline(x)
+    result = integral(fun,[xlist[0],xlist[-1]],(),TOL)
+    return result,fun
+
+# testing functions  
+def yxfun(y,x):
+    f = y**2*np.exp(-x**2)
+    return f
 
 if __name__ == "__main__":
-    test()
+    import matplotlib.pyplot as plt
     #print(np.sqrt(-1+0j)) --> 1j
+
+    funy_up = lambda x: 1 
+    funy_down = lambda x: 0
+    xspan = [0,+np.infty]
+    S = integral2D(yxfun,funy_down,funy_up,xspan,args = (),TOL = 1e-12)
+    print(S*1e10)
+    print(np.sqrt(np.pi)*1e10/6)
+
+    
